d3cd0ffbcceac957f5e2141cadef506689b7cb14
Marcio Tiene <marcio.ricardo@mettzer.com>
2022-03-15 15:28:16 -0300

    style(eslint/prettier): add eslint / prettier configs

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
-import express, { Application } from "express";
+import express, { Application } from 'express';
 
 const PORT = process.env.PORT || 4200;
 
 const app: Application = express();
 
-app.get("/ping", async (_req, res) => {
+app.get('/ping', async (_req, res) => {
   res.send({
-    message: "pong",
+    message: 'pong',
   });
 });
 
 app.listen(PORT, () => {
-  console.log("Server is running on port", PORT);
+  console.log('Server is running on port', PORT);
 });
